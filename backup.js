1b28e4db3d8b955f4fb64fed044031b6dd9f46cf
rikilega <rosedoland33@yahoo.com>
2023-04-11 20:09:24 -0700

    buttons weren't working. So trouble shoot the script. All working! added most of css. still  minor tweaks remaining.

--- backup.js
+++ backup.js
@@ -7,24 +7,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const date = dayjs().format("dddd, MMMM D, YYYY")
     const savedCityDate = JSON.parse(localStorage.getItem('savedCityDate') || "[]")
     const newSavedCityDate = savedCityDate.filter(entry => entry.date === date);
-    localStorage.setItem('newSavedCityDate', JSON.stringify(newSavedCityDate));
+    localStorage.setItem('newsavedCityDate', JSON.stringify(newSavedCityDate));
     const savedTopCities = JSON.parse(localStorage.getItem('savedTopCities') || "[]")
- 
     
-    //iterate through savedTopCities array in local storage, create buttons with textcontent from strings in array.
+    console.log(savedTopCities)
     const topCitybtns = document.getElementById("topCities");
     for (let i = 0; i < savedTopCities.length; i++) {
         const city = savedTopCities[i];
         const button = document.createElement("button");
-        button.textContent = `${city}`;
+        button.textContent = `${city.toUpperCase()}`;
         topCitybtns.appendChild(button);
-        console.log(topCitybtns)
     }
 
+    // const date = dayjs().format("dddd, MMMM D, YYYY")
     const topCities = document.getElementById("topCities");
     const searchBtns = topCities.querySelectorAll("button");
 
-    //load data in local storage from click
     searchBtns.forEach(btn => {
         btn.addEventListener('click', event => {
             console.log('click registered')
@@ -32,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const city = btn.textContent
         console.log(city);
         const newSavedCityDate = JSON.parse(localStorage.getItem('newSavedCityDate') || "[]")
-        const loadWeatherIndex = newSavedCityDate.findIndex(entry => entry.city === city && entry.date === date);
+        const loadWeatherIndex = newSavedCityDate.findIndex(entry => entry.city === city);
         console.log(loadWeatherIndex)
         // localStorage.setItem('savedCityDate', JSON.stringify(savedCityDate));
         const savedEntry = newSavedCityDate[loadWeatherIndex];
@@ -47,51 +45,46 @@ document.addEventListener("DOMContentLoaded", function() {
         localStorage.setItem('savedCityDate', JSON.stringify(savedCityDate));
         saveTopCities();
     }
-    
-    });
-            
-    });
-    });
-
-    //add event listener to initiate function for getting weather data
-    const form = document.querySelector('form')
-    const cityInput = document.getElementById('cityInput');
-    form.addEventListener('submit', event => {
-    event.preventDefault();
-    const city = cityInput.value.toUpperCase();           
-    saveSearch(city);
-    console.log("search clicked")
-    console.log(city)
-}) 
+});
+        
+});
+});
 
 
-//function to check and load saved weather data if any from the current date,  will call getWeatherData() to load new data.
 function saveSearch() {
     const date = dayjs().format("dddd, MMMM D, YYYY");
     const city = cityInput.value
+    
     const savedCityDate = JSON.parse(localStorage.getItem('savedCityDate') || "[]")
+
+    
     const duplicateEntryIndex = savedCityDate.findIndex(entry => entry.city === city && entry.date === date);
+    // const badWeatherData = (Object.keys(savedCityDate[duplicateEntryIndex].weatherData)).length === 0;
+    // savedCityDate.push({city, date})
+    // localStorage.setItem('savedCityDate', JSON.stringify(savedCityDate));
     console.log(savedCityDate)
     if (duplicateEntryIndex < 0) {
             getWeatherData(city) 
+
     } else if (duplicateEntryIndex >= 0) {
         const savedEntry = savedCityDate[duplicateEntryIndex];
         weatherDataElem.innerHTML = savedEntry.weatherData
-        savedCityDate.push({city: city.toUpperCase(), date, weatherData: savedEntry.weatherData});
+        savedCityDate.push({city, date, weatherData: savedEntry.weatherData});
         console.log("duplicateentry")
         localStorage.setItem('savedCityDate', JSON.stringify(savedCityDate));
         saveTopCities();
     }
 };
 
-
-function getWeatherData(city) {
-    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${city}`;
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
+    function getWeatherData(city) {
+        const url = `https://nominatim.openstreetmap.org/search?format=json&q=${city}`;
+        const savedCityDate = JSON.parse(localStorage.getItem('savedCityDate') || "[]")
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+      // Parse the data and extract the relevant information
         const { lat, lon } = data[0];
-        console.log(lat,lon)
+        console.log(lat, lon)
         const urlWithCoords = baseURL.replace('{lat}', lat).replace('{lon}', lon);
         const currURLWithCoords = currURL.replace('{lat}', lat).replace('{lon}', lon);
         return Promise.all([
@@ -99,64 +92,82 @@ function getWeatherData(city) {
             fetch(currURLWithCoords)
         ]);
     })
-
+    
     .then(responses => Promise.all(responses.map(response => response.json())))
     .then(data => {
         const filteredForecast = data[0].list.filter(entry => entry.dt_txt.endsWith('06:00:00'));
-        console.log(filteredForecast)
-        //call functions to parse the filtered forecasts for both current and 5day for information to display in dashboard
+        // const city = data[0].city.name;
+        console.log(filteredForecast);
+        // Parse the data and extract the relevant information
         const cardInfo = parseWeatherData(filteredForecast);
         console.log(cardInfo);
         const currentWeather = parseCurrentWeatherData(data[1]);
         console.log(currentWeather)
-        //call function to render cardInfo and current weather to HTML
-        const weatherData = renderWeatherData(cardInfo, currentWeather);
-        weatherDataElem.innerHTML = weatherData
-        
-        //save search data to storage for search buttons and loading duplicate searches from local storage
-        const saveddate = dayjs().format("dddd, MMMM D, YYYY")
-        const savedCityDate = JSON.parse(localStorage.getItem('savedCityDate') || "[]")
-        savedCityDate.push({city: city.toUpperCase(), date: saveddate, weatherData});
+        weatherDataElem.innerHTML = renderWeatherData(cardInfo, currentWeather);
+        console.log(weatherDataElem.innerHTML)
+        //const weatherData = weatherDataElem.innerHTML
+        const weatherData = weatherDataElem.innerHTML
+        const saveddate = dayjs().format("dddd, MMMM D, YYYY");
+
+        console.log(savedCityDate);
+        savedCityDate.push({city, date: saveddate, weatherData});
         console.log(savedCityDate)
         localStorage.setItem('savedCityDate', JSON.stringify(savedCityDate));
+
+        saveTopCities();
+        
     })
 
-    //call function to save top searches to local storage
-    saveTopCities()
+}  
+
+function saveTopCities() {
+    localStorage.removeItem('savedTopCities');
+    const savedCityDate = JSON.parse(localStorage.getItem('savedCityDate') || "[]")
+    const cityCounts = savedCityDate.reduce((counts, entry) => {
+    counts[entry.city] = (counts[entry.city] || 0) + 1;
+    return counts;
+}, {});
+console.log(cityCounts)
+const topCities = Object.keys(cityCounts)
+    .sort((a, b) => cityCounts[b] - cityCounts[a])
+    .slice(0, 4);
+
+console.log(topCities)
+localStorage.setItem('savedTopCities', JSON.stringify(topCities));
+
 }
 
-//parse 5day forecast data for display data for each day
-function parseWeatherData(filteredData) {
-    const cardInfo = filteredData.map(item => ({
-        date: new Date(item.dt_txt),
-        temperature: parseInt((item.main.temp - 273.15) * 9/5 + 32),
-        description: item.weather[0].description,
-        humidity: item.main.humidity + "%",
-        wind: ((item.wind.speed) * 1.15).toFixed(1) + " mph",
-        icon: item.weather[0].icon
-    }));
-    
-    return cardInfo ;// 5day forecast display data array
-}
-
-//parse current forecast data for display only data
 function parseCurrentWeatherData(data) {
+    // Parse the data and extract the relevant information
     const city = data.name;
     const temperature = parseInt((data.main.temp - 273.15) * 9/5 + 32);
     const description = data.weather[0].description;
     const icon = data.weather[0].icon;
     const wind = ((data.wind.speed) * 1.15).toFixed(1) + "mph";
     const humidity = data.main.humidity + "%";
+    console.log(data);
     return { city, temperature, description, wind, humidity, icon };
-};
+}
 
-//function to render data from cardInfo and currentWeather to HTML
+function parseWeatherData(filteredData) {
+    const cardInfo = filteredData.map(item => ({
+        date: new Date(item.dt_txt)(undefined, { dateStyle: 'short' }),
+        temperature: parseInt((item.main.temp - 273.15) * 9/5 + 32),
+        description: item.weather[0].description,
+        humidity: item.main.humidity + "%",
+        wind: ((item.wind.speed) * 1.15).toFixed(1) + " mph",
+        icon: item.weather[0].icon
+    }));
+    return cardInfo ;
+}
+
+
+
 function renderWeatherData(cardInfo, currentWeather) {
-
-    // Generate HTML for each filtered day
+    // Generate HTML for each filtered forecast item
     const forecastHTML = cardInfo.map(item => `
         <div class="forecast-item">
-            <div>${new Date(item.date).toLocaleString('en-US', { timeZone: 'UTC', year: 'numeric', month: '2-digit', day: '2-digit' })}</div>
+            <div>${new Date(item.date).toLocaleString()}</div>
             <div>${item.temperature} &deg;F</div>
             <div>${item.description}</div>
             <img src="http://openweathermap.org/img/wn/${item.icon}.png">
@@ -184,22 +195,14 @@ function renderWeatherData(cardInfo, currentWeather) {
         <h2 >5-Day Forecast for ${currentWeather.city}</h2>
         <div class="forecast">${forecastHTML}</div>
     `;
-
 }
 
-//function to rank cities by search volume and save array to local storage as savedTopCities
-function saveTopCities() {
-    localStorage.removeItem('savedTopCities');
-    const savedCityDate = JSON.parse(localStorage.getItem('savedCityDate') || "[]")
-    const cityCounts = savedCityDate.reduce((counts, entry) => {
-    counts[entry.city] = (counts[entry.city] || 0) + 1;
-    return counts;
-}, {});
-console.log(cityCounts)
-const topCities = Object.keys(cityCounts)
-    .sort((a, b) => cityCounts[b] - cityCounts[a])
-    .slice(0, 4);
 
-console.log(topCities)
-localStorage.setItem('savedTopCities', JSON.stringify(topCities));
-}
+const form = document.querySelector('form');
+const cityInput = document.getElementById('cityInput');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const city = cityInput.value;           
+    saveSearch(city);
+    console.log()
+}) 
